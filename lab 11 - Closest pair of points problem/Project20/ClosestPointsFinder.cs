44b3182b19199982e62886b2168bb0befdec9181
mikevelko <mikeveko@gmail.com>
2020-07-03 13:33:10 +0200

    lab11

---
+++ lab 11 - Closest pair of points problem/Project20/ClosestPointsFinder.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+
+namespace ASD
+{
+    [Serializable]
+    public struct Point
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+        public Point(double x, double y) { X = x; Y = y; }
+        public override string ToString() => $"({X};{Y})";
+    }
+
+    public class ClosestPointsFinder : MarshalByRefObject
+    {
+        /// <summary>
+        /// Szukanie najbliższej pary punktów (Złożoność O(n*logn)).
+        /// </summary>
+        /// <param name="points">
+        /// Tablica unikatowych punktów wśród, który wyznaczana jest najbliższa para punktów.
+        /// </param>
+        /// <returns>
+        /// Znaleziona najbliżej położona para punktów. Jeżeli jest kilka należy zwrócić dowolną.
+        /// </returns>
+        public (Point P1, Point P2) findClosestPoints(Point[] points)
+        {
+            Point[] SortedPointsX = points.OrderBy(p => p.X).ToArray();
+            
+            
+            return closestUtil(SortedPointsX, points.Length);
+        }
+        private double Distance(Point p1, Point p2)
+        {
+            return Math.Sqrt((p1.X - p2.X)*(p1.X - p2.X) + (p1.Y - p2.Y)*(p1.Y - p2.Y));
+        }
+        private (Point p1,Point p2) BruteForce(Point[] P)
+        {
+            double min = double.MaxValue;
+            (Point p1, Point p2) points = (new Point(), new Point());
+            for (int i = 0; i < P.Length; ++i)
+                for (int j = i + 1; j < P.Length; ++j)
+                    if (Distance(P[i], P[j]) < min)
+                    {
+                        min = Distance(P[i], P[j]);
+                        points = (P[i], P[j]);
+                    }
+            return points;
+        }
+        private double Min(double x, double y)
+        {
+            return (x < y) ? x : y;
+        }
+        private (Point p1, Point p2) closestUtil(Point[] Px, int n)
+        {
+            if (n <= 3)
+                return BruteForce(Px);
+ 
+            int mid = n / 2;
+            Point midPoint = Px[mid];
+
+            Point[] Pxl = new Point[mid];   
+            Point[] Pxr = new Point[n - mid];  
+            int li = 0, ri = 0;  
+            for (int i = 0; i < n; i++)
+            {
+                if (Px[i].X <= midPoint.X && li < mid)
+                    Pxl[li++] = Px[i];
+                else
+                    Pxr[ri++] = Px[i];
+            }
+
+        
+            
+            (Point left1, Point left2) = closestUtil(Pxl, mid);
+            (Point right1, Point right2) = closestUtil(Pxr, n - mid);
+
+            double dl, dr;
+            
+            dl = Distance(left1, left2);
+            dr = Distance(right1, right2); 
+            double dMin = Min(dl, dr);
+
+            (Point best1, Point best2) best;
+            if (dl < dr)
+            {
+                best = (left1, left2);
+            }
+            else 
+            {
+                best = (right1, right2);
+            }
+
+            // Build an array strip[] that contains points close (closer than d) 
+            // to the line passing through the middle point 
+            List<Point> PointList = new List<Point>();
+            for (int i = 0; i < n; i++)
+                if (Math.Abs(Px[i].X - midPoint.X) < dMin)
+                    PointList.Add(Px[i]);
+
+            Point[] Band = PointList.OrderBy(p => p.Y).ToArray();
+            
+            return stripClosest(Band, dMin,best.best1,best.best2);
+        }
+        (Point p1,Point p2) stripClosest(Point[] strip, double d,Point best1, Point best2)
+        {
+            double min = d;  // Initialize the minimum distance as d 
+            (Point p1, Point p2) points = (best1, best2);
+            // Pick all points one by one and try the next points till the difference 
+            // between y coordinates is smaller than d. 
+            // This is a proven fact that this loop runs at most 6 times 
+            for (int i = 0; i < strip.Length; ++i)
+                for (int j = i + 1; j < strip.Length && (strip[j].Y - strip[i].Y) < min; ++j)
+                    if (Distance(strip[i], strip[j]) < min)
+                    {
+                        min = Distance(strip[i], strip[j]);
+                        points = (strip[i], strip[j]);
+                    }
+            return points;
+        }
+    }
+}
---
+++ lab 11 - Closest pair of points problem/Project20/Lab11Main.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ASD
+{
+    public class ClosestPointsFinderTestCase : TestCase
+    {
+        private readonly Point[] points;
+        private readonly Point[] pointsCopy;
+
+        private readonly (Point P1, Point P2)? expectedClosestPairPoints;
+        private readonly double expectedDistance;
+        private readonly bool checkOnlyDistance;
+
+        private (Point P1, Point P2) closestPairPoints;
+
+        public ClosestPointsFinderTestCase(double timeLimit, Exception expectedException, string description, (Point[] Points, bool CheckOnlyDistance,
+            (Point P1, Point P2)? ExpectedClosestPairPoints, double ExpectedDistance) data) : base(timeLimit, expectedException, description)
+        {
+            points = data.Points;
+            pointsCopy = (Point[])points.Clone();
+            expectedClosestPairPoints = data.ExpectedClosestPairPoints;
+            expectedDistance = data.ExpectedDistance;
+            checkOnlyDistance = data.CheckOnlyDistance;
+        }
+
+        protected override void PerformTestCase(object prototypeObject)
+        {
+            var finder = (ClosestPointsFinder)prototypeObject;
+            closestPairPoints = finder.findClosestPoints(points);
+        }
+
+        protected override (Result resultCode, string message) VerifyTestCase(object settings)
+        {
+            if (!points.SequenceEqual(pointsCopy))
+            {
+                return (Result.WrongResult, "Input data was modified");
+            }
+
+            if (!checkOnlyDistance)
+            {
+                if (!((expectedClosestPairPoints.Value.P1.Equals(closestPairPoints.P1) && expectedClosestPairPoints.Value.P2.Equals(closestPairPoints.P2)) ||
+                    (expectedClosestPairPoints.Value.P2.Equals(closestPairPoints.P1) && expectedClosestPairPoints.Value.P1.Equals(closestPairPoints.P2))))
+                    return (Result.WrongResult, $"Wrong closest points! Expected result: {expectedClosestPairPoints}, your result: {closestPairPoints}");
+            }
+
+            double distance = LabTests.Length(closestPairPoints);
+
+            if (Math.Abs(expectedDistance - LabTests.Length(closestPairPoints)) > 1e-4)
+            {
+                return (Result.WrongResult, $"Wrong distance! Expected result: {expectedDistance:F6}, your result: {distance:F6}");
+            }
+
+            return (Result.Success, $"OK, time: {PerformanceTime:F3}");
+        }
+
+
+    }
+
+    public static class LabTests
+    {
+        public static double Length((Point P1, Point P2) segment)
+        {
+            double dx = segment.P1.X - segment.P2.X;
+            double dy = segment.P1.Y - segment.P2.Y;
+            return Math.Sqrt(dx * dx + dy * dy);
+        }
+
+        public static (Point[] Points, bool CheckOnlyDistance, (Point P1, Point P2)? ExpectedClosestPairPoints, double ExpectedDistance) TwoPointsTest()
+        {
+            //2 punkty
+            var points = new[] { new Point(10, 10), new Point(11, 11) };
+            var properResult = (new Point(10, 10), new Point(11, 11));
+
+            return (points, false, properResult, 1.4142135623731);
+        }
+
+        public static (Point[] Points, bool CheckOnlyDistance, (Point P1, Point P2)? ExpectedClosestPairPoints, double ExpectedDistance) SameXTest()
+        {
+            //wszystkie z tą samą x-owa wspołrzędną
+            var points = new[] { new Point(10, 9.5), new Point(10, 10), new Point(10, 11), new Point(10, 12), new Point(10, 13), new Point(10, 14), new Point(10, 15), new Point(10, 16) };
+            var properResult = (new Point(10, 9.5), new Point(10, 10));
+
+            return (points, false, properResult, 0.5);
+        }
+
+        public static (Point[] Points, bool CheckOnlyDistance, (Point P1, Point P2)? ExpectedClosestPairPoints, double ExpectedDistance) SameYTest()
+        {
+            //wszystkie z tą samą y-owa wspołrzędną
+            var points = new[] { new Point(1, 10), new Point(2, 10), new Point(3.5, 10), new Point(4, 10), new Point(5, 10), new Point(6, 10) };
+            var properResult = (new Point(3.5, 10), new Point(4, 10));
+
+            return (points, false, properResult, 0.5);
+        }
+
+        public static (Point[] Points, bool CheckOnlyDistance, (Point P1, Point P2)? ExpectedClosestPairPoints, double ExpectedDistance) ThreePointsTest()
+        {
+            //3 punkty
+            var points = new[] { new Point(10, 10), new Point(11, 11), new Point(15, -17) };
+            var properResult = (new Point(10, 10), new Point(11, 11));
+
+            return (points, false, properResult, 1.4142135623731);
+        }
+
+        public static (Point[] Points, bool CheckOnlyDistance, (Point P1, Point P2)? ExpectedClosestPairPoints, double ExpectedDistance) SquareTest()
+        {
+            //kwadrat
+            var points = new[] { new Point(10, 10), new Point(-10, 10), new Point(10, -10), new Point(-10, -10) };
+            var properResult = (new Point(10, 10), new Point(-10, 10));
+
+            return (points, true, properResult, 20);
+        }
+
+        public static (Point[] Points, bool CheckOnlyDistance, (Point P1, Point P2)? ExpectedClosestPairPoints, double ExpectedDistance) RandomTest(int count, int minX = -100, int maxX = 100, int minY = -100, int maxY = 100, double xFactor = 1, double yFactor = 1, int seed = 1024, double eps = 1e-4F)
+        {
+            //chmura punktow, najblizsze generowane automatycznie
+            Random random = new Random(seed);
+            var points = Enumerable.Repeat(0, count).Select(i => new Point(random.Next(minX, maxX), random.Next(minY, maxY))).Distinct().ToList();
+            var p1 = points[random.Next(0, points.Count())];
+            var p2 = new Point(p1.X + xFactor * eps, p1.Y + yFactor * eps);
+            points.Add(p2);
+            var properResult = (p1, p2);
+
+            return (points.ToArray(), false, properResult, LabTests.Length(properResult));
+        }
+    }
+
+    public class Lab11TestModule : TestModule
+    {
+        public override void PrepareTestSets()
+        {
+            var subjectUnderTest = new ClosestPointsFinder();
+
+            var labClosestPairTests = new TestSet(
+                subjectUnderTest,
+                "Laboratorium - Szukanie najbliższej pary punktów"
+            );
+            labClosestPairTests.TestCases.AddRange(new List<TestCase>
+            {
+                new ClosestPointsFinderTestCase(1, null, "Dwa punkty", LabTests.TwoPointsTest()),
+                new ClosestPointsFinderTestCase(1, null, "Taka sama współrzędna x", LabTests.SameXTest()),
+                new ClosestPointsFinderTestCase(1, null, "Taka sama współrzędna y", LabTests.SameYTest()),
+                new ClosestPointsFinderTestCase(1, null, "Trzy punkty", LabTests.ThreePointsTest()),
+                new ClosestPointsFinderTestCase(1, null, "Kwadrat", LabTests.SquareTest()),
+                new ClosestPointsFinderTestCase(1, null, "500 losowych punktów", LabTests.RandomTest(500, yFactor: -2)),
+                new ClosestPointsFinderTestCase(1, null, "1000 losowych punktów", LabTests.RandomTest(1000, yFactor: -2)),
+                new ClosestPointsFinderTestCase(1, null, "1500 losowych punktów", LabTests.RandomTest(1500, yFactor: -2)),
+                new ClosestPointsFinderTestCase(1, null, "2000 losowych punktów", LabTests.RandomTest(2000, yFactor: -2)),
+                new ClosestPointsFinderTestCase(3, null, "50000 losowych punktów", LabTests.RandomTest(50000, -10000, 10000, yFactor: -2)),
+            });
+
+            TestSets = new Dictionary<string, TestSet>
+            {
+                {"LabClosestPointsTests", labClosestPairTests},
+            };
+        }
+
+        public override double ScoreResult()
+        {
+            return 3.0;
+        }
+
+    }
+
+    public class Lab11Main
+    {
+        public static void Main()
+        {
+            var testModule = new Lab11TestModule();
+            testModule.PrepareTestSets();
+            foreach (var testSet in testModule.TestSets)
+            {
+                testSet.Value.PerformTests(verbose: true, checkTimeLimit: false);
+            }
+        }
+    }
+}
